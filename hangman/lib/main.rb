cb946c018597c807c0ee6673ebd7355902ab33ac
jamjamsnugglepuff <jaydenvausemailbox@gmail.com>
2020-08-23 15:04:40 +1000

    added in hangman game ruby io class using file open/load

--- hangman/lib/main.rb
+++ hangman/lib/main.rb
@@ -1,13 +1,23 @@
+require 'yaml'
 class Game
+  attr_accessor :code, :hint, :guesses, :file_name
 
   def initialize
-    @words = File.open("5desk.txt")
+    @words = File.open("../5desk.txt")
     @code = generate_word_array(@words).chomp.downcase.split("")
     @hint = Array.new(@code.length,"-")
     @guesses = 10
+    @file_name = "save.yml"
   end
 
   def run_game
+    puts "player would you like to load game Y/N ?"
+    load_game_choice = gets.chomp.upcase
+      if load_game_choice == "Y"
+        load_game(@file_name, self)
+        puts "save game"
+      end
+
     puts "player the rules of the game are within 10 rounds you must guess the word in the code, (displayed in the hints that are given based off your answers) if you dont do that within the given rounds then the game is over and you lose , be smart and use the hints given as much as possible goodluck!"
     while @guesses >= 0 do
       self.display_hint_ui
@@ -40,6 +50,13 @@ class Game
   end
 # get player input to check if its in the code
   def get_player_input
+    puts "player would you like to save your game? Y/N"
+    save_game_choice = gets.chomp.upcase
+      if save_game_choice == "Y"
+        save_game(@file_name, self)
+        puts "save game"
+      end
+    
     puts "player give an input of a-z"
     print "Player:"
     input = gets.chomp.to_s.downcase
@@ -66,11 +83,51 @@ class Game
     # self.display_hint_ui
   end
 
+ def save_game(file_name, game_state)
+  File.open("../#{file_name}", "w"){
+    |file|
+    
+    file.write(game_state.to_yaml)
+  }
+end
+
+
+  def load_game(file_name, game)
+  contents = YAML.load(File.read("../#{file_name}"))
+    self.code = contents.code
+    self.hint = contents.hint
+    self.guesses = contents.guesses
+  end
+
 end
 
 game = Game.new
 # game.display_hint_ui
 # game.get_player_input
 # game.does_include?(game.get_player_input)
+
+# x =  game.to_json
+# p x
+# puts " "
+# p game.from_json(x)
+ 
+
+# def save_game(file_name, game_state)
+#   File.open(file_name, "w"){
+#     |file|
+    
+#     file.write(game_state.to_yaml)
+#   }
+# end
+
+# def load_game(file_name, game)
+#  contents = YAML.load(File.read(file_name))
+#    game = contents
+#  end
+
+# save_game(file_name, game)
+# game = load_game(file_name, game)
+
+# puts YAML::load(serialized_cla= 
 
 game.run_game
